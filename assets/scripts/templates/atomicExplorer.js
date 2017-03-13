0cb0b13421de1a2e44d0bc05abfa2f8646dedb5c
pbca26 <pbca26@gmail.com>
2017-03-13 18:36:45 +0300

    reduce selector copy-paste code to template

--- assets/scripts/templates/atomicExplorer.js
+++ assets/scripts/templates/atomicExplorer.js
@@ -6,7 +6,7 @@ templates.atomicExplorer =
     <div class="row" id="atomic-explorer" data-plugin="masonry">
       <div class="col-xlg-12 col-md-12">
         <h4 class="font-size-14 text-uppercase">Atomic Explorer</h4>
-        <!-- Atomic Expolrer Tab -->
+        <!-- Atomic Explorer Tab -->
         <div class="panel panel-bordered">
           <!--<div class="panel-heading">
             <h3 class="panel-title">Panel Heading</h3>
@@ -15,71 +15,8 @@ templates.atomicExplorer =
             <div class="col-sm-4 col-xs-12">
               <div class="form-group">
                 <select class="form-control form-material" id="atomic_explorer_select_coin_options">
-                  <option value="">-Select Coin-</option>
-                  <option value="ANC">AnonCoin</option> 
-                  <option value="BTC">Bitcoin</option>
-                  <option value="BTCD">BitcoinDark</option>
-                  <option value="BTM">Bitmark</option>
-                  <option value="CARB">Carboncoin</option>
-                  <option value="DGB">Digibyte</option>
-                  <option value="DOGE">Dogecoin</option>
-                  <option value="FRK">Franko</option>
-                  <option value="KMD">Komodo</option>
-                  <option value="LTC">Litecoin</option>
-                  <option value="MZC">MazaCoin</option>
-                  <option value="SYS">SysCoin</option>
-                  <option value="UNO">Unobtanium</option>
-                  <option value="ZEC">Zcash</option>
-                  <option value="ZET">Zetacoin</option>
-                  <option value="BET">BET</option>
-                  <option value="BOTS">BOTS</option>
-                  <option value="CEAL">CEAL NET</option>
-                  <option value="CRYPTO">CRYPTO</option>
-                  <option value="HODL">HODL</option>
-                  <option value="DEX">InstantDEX</option>
-                  <option value="JUMBLR">JUMBLR</option>
-                  <option value="KV">KV</option>
-                  <option value="MGW">MultiGateway</option>
-                  <option value="MVP">MVP Lineup</option>
-                  <option value="PANGEA">PANGEA</option>
-                  <option value="REVS">REVS</option>
-                  <option value="SHARK">SHARK</option>
-                  <option value="MESH">SpaceMesh</option>
-                  <option value="SUPERNET">SUPERNET</option>
-                  <option value="WIRELESS">WIRELESS</option>
-                  <option data-lang="CURRENCIES"></option>
-                  <option value="AUD">Australian Dollar</option>
-                  <option value="BRL">Brazilian Real</option>
-                  <option value="GBP">British Pound</option>
-                  <option value="BGN">Bulgarian Lev</option>
-                  <option value="CAD">Canadian Dollar</option>
-                  <option value="HRK">Croatian Kuna</option>
-                  <option value="CZK">Czech Koruna</option>
-                  <option value="CNY">Chinese Yuan</option>
-                  <option value="DKK">Danish Krone</option>
-                  <option value="EUR">Euro</option>
-                  <option value="HKD">Hong Kong Dollar</option>
-                  <option value="HUF">Hungarian Forint</option>
-                  <option value="INR">Indian Rupee</option>
-                  <option value="IDR">Indonesian Rupiah</option>
-                  <option value="ILS">Israeli Shekel</option>
-                  <option value="JPY">Japanese Yen</option>
-                  <option value="KRW">Korean Won</option>
-                  <option value="MYR">Malaysian Ringgit</option>
-                  <option value="MXN">Mexican peso</option>
-                  <option value="NZD">New Zealand Dollar</option>
-                  <option value="NOK">Norwegian Krone</option>
-                  <option value="PHP">Philippine Peso</option>
-                  <option value="PLN">Polish Zloty</option>
-                  <option value="RON">Romanian Leu</option>
-                  <option value="RUB">Russian Ruble</option>
-                  <option value="SGD">Singapore Dollar</option>
-                  <option value="ZAR">South African Rand</option>
-                  <option value="SEK">Swedish Krona</option>
-                  <option value="CHF">Swiss Franc</option>
-                  <option value="THB">Thai Baht</option>
-                  <option value="TRY">Turkish Lira</option>
-                  <option value="USD">US Dollar</option>
+                  <option value="" data-lang="INDEX.SELECT_COIN"></option>
+                  ${templates.addCoinOptions}
                 </select>
               </div>
             </div>
@@ -126,7 +63,7 @@ templates.atomicExplorer =
             </div>
           </div>
         </div>
-        <!-- End Atomic Expolrer Tab -->
+        <!-- End Atomic Explorer Tab -->
       </div>
       <div class="col-xlg-4 col-md-12">
         <!-- Atomic Explorer tab -->
--- assets/scripts/templates/bottomIncludes.js
+++ assets/scripts/templates/bottomIncludes.js
@@ -1,135 +1,135 @@
 templates.bottomIncludes =
 `
-    <script>if (typeof module === 'object') {window.module = module; module = undefined;}</script>
-    <script src="assets/global/vendor/jquery/jquery.min.js"></script>
-    <script src="assets/global/vendor/bootstrap/bootstrap.min.js"></script>
-    <script src="assets/global/vendor/animsition/animsition.min.js"></script>
-    <script src="assets/global/vendor/asscroll/jquery-asScroll.min.js"></script>
-    <script src="assets/global/vendor/jquery-scrollbar/jquery.scrollbar.min.js"></script>
-    <script src="assets/global/vendor/mousewheel/jquery.mousewheel.min.js"></script>
-    <script src="assets/global/vendor/asscrollable/jquery.asScrollable.all.min.js"></script>
-    <script src="assets/global/vendor/ashoverscroll/jquery-asHoverScroll.min.js"></script>
-    <script src="assets/global/vendor/waves/waves.min.js"></script>
-    <!-- Plugins -->
-    <script src="assets/global/vendor/switchery/switchery.min.js"></script>
-    <script src="assets/global/vendor/intro-js/intro.min.js"></script>
-    <script src="assets/global/vendor/screenfull/screenfull.min.js"></script>
-    <script src="assets/global/vendor/slidepanel/jquery-slidePanel.min.js"></script>
-    <script src="assets/global/vendor/bootbox/bootbox.js"></script>
-    <script src="assets/global/vendor/jquery-validation/jquery.validate.min.js"></script>
-    <script src="assets/global/vendor/jquery-validation/additional-methods.min.js"></script>
-    <script src="assets/global/vendor/bootstrap-toastr/toastr.min.js"></script>
-    <script src="assets/global/vendor/bootstrap-select/bootstrap-select.min.js"></script>
-    <script src="assets/global/vendor/sweetalert2/sweetalert2.min.js" type="text/javascript"></script>
-    <script src="assets/global/vendor/jquery-qrcode/jquery.qrcode.min.js" type="text/javascript"></script>
-    <script src="assets/global/vendor/clipboard/clipboard.min.js"></script>
-    <script src="assets/global/vendor/asprogress/jquery-asProgress.min.js"></script>
-    <script src="assets/global/vendor/jquery-labelauty/jquery-labelauty.js"></script>
-    <script src="assets/global/vendor/alertify-js/alertify.js"></script>
-    <script src="assets/global/vendor/nprogress/nprogress.js"></script>
-    <script src="assets/global/vendor/ladda-bootstrap/spin.min.js"></script>
-    <script src="assets/global/vendor/ladda-bootstrap/ladda.min.js"></script>
-    <script src="assets/global/vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="assets/global/vendor/datatables-fixedheader/dataTables.fixedHeader.js"></script>
-    <script src="assets/global/vendor/datatables-bootstrap/dataTables.bootstrap.min.js"></script>
-    <script src="assets/global/vendor/datatables-responsive/dataTables.responsive.js"></script>
-    <script src="assets/global/vendor/datatables-tabletools/dataTables.tableTools.js"></script>
-    <script src="assets/global/vendor/underscore-js/underscore-min.js"></script>
-    <script src="assets/global/vendor/javascript-md5/md5.min.js"></script>
-    <script src="assets/global/vendor/bluebird/bluebird.min.js"></script>
-    <script src="assets/global/vendor/socket.io.js"></script>
-    <!-- Crypto Dependencies -->
-    <script src="assets/scripts/wordlist.js" type="text/javascript"></script>
-    <script src="assets/scripts/passphrasegenerator.js" type="text/javascript"></script>
-    <script src="assets/scripts/seedrandom.js" type="text/javascript"></script>
-    <!-- Scripts -->
-    <script src="assets/global/js/core.js"></script>
-    <script src="assets/js/site.js"></script>
-    <script src="assets/js/sections/menu.js"></script>
-    <script src="assets/js/sections/menubar.js"></script>
-    <script src="assets/js/sections/sidebar.js"></script>
-    <script src="assets/global/js/configs/config-colors.js"></script>
-    <script src="assets/js/configs/config-tour.js"></script>
-    <script src="assets/global/js/components/asscrollable.js"></script>
-    <script src="assets/global/js/components/nprogress.min.js"></script>
-    <script src="assets/global/js/components/animsition.js"></script>
-    <script src="assets/global/js/components/slidepanel.js"></script>
-    <script src="assets/global/js/components/bootbox.js"></script>
-    <script src="assets/global/js/components/switchery.js"></script>
-    <script src="assets/global/js/components/tabs.js"></script>
-    <script src="assets/global/js/components/buttons.js"></script>
-    <script src="assets/global/js/components/ladda-bootstrap.js"></script>
-    <script src="assets/global/js/components/asprogress.min.js"></script>
-    <script src="assets/global/js/components/jquery-labelauty.min.js"></script>
-    <script src="assets/global/js/components/alertify-js.js"></script>
-    <script src="assets/global/js/components/panel.min.js"></script>
-    <script src="assets/scripts/config.js"></script>
-    <script src="assets/scripts/checkie.js"></script>
+<script>if (typeof module === 'object') {window.module = module; module = undefined;}</script>
+<script src="assets/global/vendor/jquery/jquery.min.js"></script>
+<script src="assets/global/vendor/bootstrap/bootstrap.min.js"></script>
+<script src="assets/global/vendor/animsition/animsition.min.js"></script>
+<script src="assets/global/vendor/asscroll/jquery-asScroll.min.js"></script>
+<script src="assets/global/vendor/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+<script src="assets/global/vendor/mousewheel/jquery.mousewheel.min.js"></script>
+<script src="assets/global/vendor/asscrollable/jquery.asScrollable.all.min.js"></script>
+<script src="assets/global/vendor/ashoverscroll/jquery-asHoverScroll.min.js"></script>
+<script src="assets/global/vendor/waves/waves.min.js"></script>
+<!-- Plugins -->
+<script src="assets/global/vendor/switchery/switchery.min.js"></script>
+<script src="assets/global/vendor/intro-js/intro.min.js"></script>
+<script src="assets/global/vendor/screenfull/screenfull.min.js"></script>
+<script src="assets/global/vendor/slidepanel/jquery-slidePanel.min.js"></script>
+<script src="assets/global/vendor/bootbox/bootbox.js"></script>
+<script src="assets/global/vendor/jquery-validation/jquery.validate.min.js"></script>
+<script src="assets/global/vendor/jquery-validation/additional-methods.min.js"></script>
+<script src="assets/global/vendor/bootstrap-toastr/toastr.min.js"></script>
+<script src="assets/global/vendor/bootstrap-select/bootstrap-select.min.js"></script>
+<script src="assets/global/vendor/sweetalert2/sweetalert2.min.js" type="text/javascript"></script>
+<script src="assets/global/vendor/jquery-qrcode/jquery.qrcode.min.js" type="text/javascript"></script>
+<script src="assets/global/vendor/clipboard/clipboard.min.js"></script>
+<script src="assets/global/vendor/asprogress/jquery-asProgress.min.js"></script>
+<script src="assets/global/vendor/jquery-labelauty/jquery-labelauty.js"></script>
+<script src="assets/global/vendor/alertify-js/alertify.js"></script>
+<script src="assets/global/vendor/nprogress/nprogress.js"></script>
+<script src="assets/global/vendor/ladda-bootstrap/spin.min.js"></script>
+<script src="assets/global/vendor/ladda-bootstrap/ladda.min.js"></script>
+<script src="assets/global/vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="assets/global/vendor/datatables-fixedheader/dataTables.fixedHeader.js"></script>
+<script src="assets/global/vendor/datatables-bootstrap/dataTables.bootstrap.min.js"></script>
+<script src="assets/global/vendor/datatables-responsive/dataTables.responsive.js"></script>
+<script src="assets/global/vendor/datatables-tabletools/dataTables.tableTools.js"></script>
+<script src="assets/global/vendor/underscore-js/underscore-min.js"></script>
+<script src="assets/global/vendor/javascript-md5/md5.min.js"></script>
+<script src="assets/global/vendor/bluebird/bluebird.min.js"></script>
+<script src="assets/global/vendor/socket.io.js"></script>
+<!-- Crypto Dependencies -->
+<script src="assets/scripts/wordlist.js" type="text/javascript"></script>
+<script src="assets/scripts/passphrasegenerator.js" type="text/javascript"></script>
+<script src="assets/scripts/seedrandom.js" type="text/javascript"></script>
+<!-- Scripts -->
+<script src="assets/global/js/core.js"></script>
+<script src="assets/js/site.js"></script>
+<script src="assets/js/sections/menu.js"></script>
+<script src="assets/js/sections/menubar.js"></script>
+<script src="assets/js/sections/sidebar.js"></script>
+<script src="assets/global/js/configs/config-colors.js"></script>
+<script src="assets/js/configs/config-tour.js"></script>
+<script src="assets/global/js/components/asscrollable.js"></script>
+<script src="assets/global/js/components/nprogress.min.js"></script>
+<script src="assets/global/js/components/animsition.js"></script>
+<script src="assets/global/js/components/slidepanel.js"></script>
+<script src="assets/global/js/components/bootbox.js"></script>
+<script src="assets/global/js/components/switchery.js"></script>
+<script src="assets/global/js/components/tabs.js"></script>
+<script src="assets/global/js/components/buttons.js"></script>
+<script src="assets/global/js/components/ladda-bootstrap.js"></script>
+<script src="assets/global/js/components/asprogress.min.js"></script>
+<script src="assets/global/js/components/jquery-labelauty.min.js"></script>
+<script src="assets/global/js/components/alertify-js.js"></script>
+<script src="assets/global/js/components/panel.min.js"></script>
+<script src="assets/scripts/config.js"></script>
+<script src="assets/scripts/checkie.js"></script>
 
-    <!-- Iguana API Scripts -->
-    <script src="assets/scripts/iguana_api.js"></script>
-    <script src="assets/scripts/iguana_api/IguanaUnused.js"></script>
-    <script src="assets/scripts/iguana_api/IguanaHandle.js"></script>
-    <script src="assets/scripts/iguana_api/IguanaAddcoin.js"></script>
-    <script src="assets/scripts/iguana_api/IguanaEDEX.js"></script>
-    <script src="assets/scripts/iguana_api/IguanaDEX.js"></script>
-    <script src="assets/scripts/iguana_api/IguanaShepherd.js"></script>
-    <script src="assets/scripts/iguana_api/IguanaJumblr.js"></script>
-    <!-- End Iguana API Scripts -->
+<!-- Iguana API Scripts -->
+<script src="assets/scripts/iguana_api.js"></script>
+<script src="assets/scripts/iguana_api/IguanaUnused.js"></script>
+<script src="assets/scripts/iguana_api/IguanaHandle.js"></script>
+<script src="assets/scripts/iguana_api/IguanaAddcoin.js"></script>
+<script src="assets/scripts/iguana_api/IguanaEDEX.js"></script>
+<script src="assets/scripts/iguana_api/IguanaDEX.js"></script>
+<script src="assets/scripts/iguana_api/IguanaShepherd.js"></script>
+<script src="assets/scripts/iguana_api/IguanaJumblr.js"></script>
+<!-- End Iguana API Scripts -->
 
-    <script src="assets/scripts/login.js"></script>
-    <!-- Dashboard Scripts -->
-    <script src="assets/scripts/dashboard.js"></script>
-    <script src="assets/scripts/dashboard/DashboardEDEX.js"></script>
-    <script src="assets/scripts/dashboard/DashboardDEX.js"></script>
-    <script src="assets/scripts/dashboard/DashboardBasilisk.js"></script>
-    <script src="assets/scripts/dashboard/DashboardCoinFiat.js"></script>
-    <!-- End Dashboard Scripts -->
+<script src="assets/scripts/login.js"></script>
+<!-- Dashboard Scripts -->
+<script src="assets/scripts/dashboard.js"></script>
+<script src="assets/scripts/dashboard/DashboardEDEX.js"></script>
+<script src="assets/scripts/dashboard/DashboardDEX.js"></script>
+<script src="assets/scripts/dashboard/DashboardBasilisk.js"></script>
+<script src="assets/scripts/dashboard/DashboardCoinFiat.js"></script>
+<!-- End Dashboard Scripts -->
 
-    <!--<script src="assets/scripts/coinwallets.js"></script>-->
-    <script src="assets/scripts/jumblr.js"></script>
-    <script src="assets/scripts/sidebar.js"></script>
-    <script src="assets/scripts/walletsettings.js"></script>
-    <script src="assets/scripts/atomicexplorer.js"></script>
+<!--<script src="assets/scripts/coinwallets.js"></script>-->
+<script src="assets/scripts/jumblr.js"></script>
+<script src="assets/scripts/sidebar.js"></script>
+<script src="assets/scripts/walletsettings.js"></script>
+<script src="assets/scripts/atomicexplorer.js"></script>
 
-    <!-- Native KMD Wallet Scripts -->
-    <script src="assets/scripts/kmd_wallet/KMDWalletDashboard.js"></script>
-    <script src="assets/scripts/kmd_wallet/KMDInit.js"></script>
-    <script src="assets/scripts/kmd_wallet/KMDBalance.js"></script>
-    <script src="assets/scripts/kmd_wallet/KMDGetinfo.js"></script>
-    <script src="assets/scripts/kmd_wallet/KMDTransaction.js"></script>
-    <script src="assets/scripts/kmd_wallet/KMDTransactionInfo.js"></script>
-    <script src="assets/scripts/kmd_wallet/KMDAddress.js"></script>
-    <script src="assets/scripts/kmd_wallet/KMDSendTx.js"></script>
-    <script src="assets/scripts/kmd_wallet_dashboard.js"></script>
-    <!-- End Native KMD Wallet Scripts -->
+<!-- Native KMD Wallet Scripts -->
+<script src="assets/scripts/kmd_wallet/KMDWalletDashboard.js"></script>
+<script src="assets/scripts/kmd_wallet/KMDInit.js"></script>
+<script src="assets/scripts/kmd_wallet/KMDBalance.js"></script>
+<script src="assets/scripts/kmd_wallet/KMDGetinfo.js"></script>
+<script src="assets/scripts/kmd_wallet/KMDTransaction.js"></script>
+<script src="assets/scripts/kmd_wallet/KMDTransactionInfo.js"></script>
+<script src="assets/scripts/kmd_wallet/KMDAddress.js"></script>
+<script src="assets/scripts/kmd_wallet/KMDSendTx.js"></script>
+<script src="assets/scripts/kmd_wallet_dashboard.js"></script>
+<!-- End Native KMD Wallet Scripts -->
 
-    <!-- Multilang Scripts -->
-    <script src="assets/scripts/lang.js"></script>
-    <script src="assets/scripts/lang/en.js"></script>
-    <!-- End Multilang Scripts -->
+<!-- Multilang Scripts -->
+<script src="assets/scripts/lang.js"></script>
+<script src="assets/scripts/lang/en.js"></script>
+<!-- End Multilang Scripts -->
 
-    <script type="text/javascript">
-      (function(document, window, $) {
-        'use strict';
-        var Site = window.Site;
-        $(document).ready(function() {
-          Site.run();
+<script type="text/javascript">
+  (function(document, window, $) {
+    'use strict';
+    var Site = window.Site;
+    $(document).ready(function() {
+      Site.run();
 
-          var socket = io.connect('http://localhost:17777');
-          socket.on('connect', function(data) {
-            socket.emit('join', 'EDEX GUI joined');
-          });
-          socket.on('messages', function(data) {
-            console.log(data);
-          });
-
-          lang();
-        });
-      })(document, window, jQuery);
-
-      jQuery(document).ready(function() {
-        jQuery('.scrollbar-dynamic').scrollbar();
+      var socket = io.connect('http://localhost:17777');
+      socket.on('connect', function(data) {
+        socket.emit('join', 'EDEX GUI joined');
       });
-    </script>
+      socket.on('messages', function(data) {
+        console.log(data);
+      });
+
+      lang();
+    });
+  })(document, window, jQuery);
+
+  jQuery(document).ready(function() {
+    jQuery('.scrollbar-dynamic').scrollbar();
+  });
+</script>
 `;
