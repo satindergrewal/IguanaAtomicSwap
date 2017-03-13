c2ebe0e355038290defda53d8356d5e4000e39ab
pbca26 <pbca26@gmail.com>
2017-03-13 22:37:41 +0300

    generalize IguanaJumblr.js

--- assets/scripts/iguana_api/IguanaJumblr.js
+++ assets/scripts/iguana_api/IguanaJumblr.js
@@ -1,42 +1,37 @@
-function Iguana_Jumblr_SetPassphrase(data) {
-  return new Promise((resolve) => {
-    var tmpIguanaRPCAuth = 'tmpIgRPCUser@' + sessionStorage.getItem('IguanaRPCAuth'),
-        ajax_data = {
-          'userpass': tmpIguanaRPCAuth,
-          'agent': 'jumblr',
-          'method': 'setpassphrase',
-          'passphrase': data.passphrase
-        };
+function Generic_Iguana_Jumblr(ajax_data, resolve) {
+  var tmpIguanaRPCAuth = 'tmpIgRPCUser@' + sessionStorage.getItem('IguanaRPCAuth');
 
-    $.ajax({
-      data: JSON.stringify(ajax_data),
-      url: 'http://127.0.0.1:' + config.iguanaPort,
-      type: 'POST',
-      dataType: 'json'
-    })
-    .done(function(data) {
-      resolve(data);
-    });
+  ajax_data.userpass = tmpIguanaRPCAuth;
+  $.ajax({
+    data: JSON.stringify(ajax_data),
+    url: 'http://127.0.0.1:' + config.iguanaPort,
+    type: 'POST',
+    dataType: 'json'
+  })
+  .done(function(data) {
+    resolve(data);
+  });
+}
+
+function Iguana_Jumblr_SetPassphrase(data) {
+  var ajax_data = {
+        'agent': 'jumblr',
+        'method': 'setpassphrase',
+        'passphrase': data.passphrase
+      };
+
+  return new Promise((resolve) => {
+    Generic_Iguana_Jumblr(ajax_data, resolve);
   });
 }
 
 function Iguana_Jumblr_Status() {
-  return new Promise((resolve) => {
-    var tmpIguanaRPCAuth = 'tmpIgRPCUser@' + sessionStorage.getItem('IguanaRPCAuth'),
-        ajax_data = {
-          'userpass': tmpIguanaRPCAuth,
-          'agent': 'jumblr',
-          'method': 'status'
-        };
+  var ajax_data = {
+        'agent': 'jumblr',
+        'method': 'status'
+      };
 
-    $.ajax({
-      data: JSON.stringify(ajax_data),
-      url: 'http://127.0.0.1:' + config.iguanaPort,
-      type: 'POST',
-      dataType: 'json'
-    })
-    .done(function(data) {
-      resolve(data);
-    });
+  return new Promise((resolve) => {
+    Generic_Iguana_Jumblr(ajax_data, resolve);
   });
 }
