f7f0ce97fe6d4d6bb0861b8d640ad2e54f81af8b
pbca26 <pbca26@gmail.com>
2017-05-07 11:13:36 +0300

    add multiple coins at once

--- react/src/actions/actionCreators.js
+++ react/src/actions/actionCreators.js
@@ -2521,6 +2521,41 @@ export function stopIguanaFork(pmid) {
   }
 }
 
+export function shepherdGetCoinList() {
+  return new Promise((resolve, reject) => {
+    fetch('http://127.0.0.1:' + Config.agamaPort + '/shepherd/coinslist', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .catch(function(error) {
+      console.log(error);
+      dispatch(triggerToaster(true, 'shepherdGetCoinList', 'Error', 'error'));
+    })
+    .then(response => response.json())
+    .then(json => resolve(json))
+  });
+}
+
+export function shepherdPostCoinList(data) {
+  return new Promise((resolve, reject) => {
+    fetch('http://127.0.0.1:' + Config.agamaPort + '/shepherd/coinslist', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ 'payload': data }),
+    })
+    .catch(function(error) {
+      console.log(error);
+      dispatch(triggerToaster(true, 'shepherdPostCoinList', 'Error', 'error'));
+    })
+    .then(response => response.json())
+    .then(json => resolve(json))
+  });
+}
+
 /*function Shepherd_SendPendValue() {
   Shepherd_SysInfo().then(function(result){
     var ram_data = formatBytes(result.totalmem_bytes)
