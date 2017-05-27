b4b548e4d34d0a0cf3422645c96d263bc0b77ee4
pbca26 <pbca26@gmail.com>
2017-05-27 11:35:41 +0300

    removed unused assets

--- react/src/assets/scripts/checkie.js
+++
@@ -1,5 +0,0 @@
-if (navigator.appName == 'Microsoft Internet Explorer') {
-	$('#section-login').fadeOut();
-	$('#section-ie-warning').fadeIn();
-	$(body).css('background-color', '#757575');
-}
--- react/src/assets/scripts/templates/nprogressBar.js
+++
@@ -1,7 +0,0 @@
-templates.nprogressBar =
-`
-<div class="bar nprogress-bar-header nprogress-bar-info" role="bar"></div>
-<div class="spinner" role="spinner">
-  <div class="spinner-icon"></div>
-</div>
-`;
