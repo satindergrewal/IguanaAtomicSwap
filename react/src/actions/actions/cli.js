a3dcbcc3065f9cb83e935ff6064a29504fcd7403
Petr Balashov <pbca26@gmail.com>
2017-06-06 18:35:24 +0200

    missing cli.js

---
+++ react/src/actions/actions/cli.js
@@ -0,0 +1,65 @@
+import {
+  triggerToaster,
+  Config
+} from '../actionCreators';
+import { CLI } from '../storeType';
+import {
+  logGuiHttp,
+  guiLogState
+} from './log';
+
+export function shepherdCliPromise(mode, chain, cmd) {
+  const _payload = {
+    mode,
+    chain,
+    cmd
+  };
+
+  return new Promise((resolve, reject) => {
+    fetch(`http://127.0.0.1:${Config.agamaPort}/shepherd/cli`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ 'payload': _payload }),
+    })
+    .catch(function(error) {
+      console.log(error);
+      dispatch(triggerToaster(true, 'shepherdCli', 'Error', 'error'));
+    })
+    .then(response => response.json())
+    .then(json => resolve(json))
+  });
+}
+
+export function shepherdCli(mode, chain, cmd) {
+  const _payload = {
+    mode,
+    chain,
+    cmd
+  };
+
+  return dispatch => {
+    return fetch(`http://127.0.0.1:${Config.agamaPort}/shepherd/cli`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ 'payload': _payload }),
+    })
+    .catch(function(error) {
+      console.log(error);
+      dispatch(triggerToaster(true, 'shepherdCli', 'Error', 'error'));
+    })
+    .then(response => response.json())
+    .then(json => cliResponseState(json))
+  }
+}
+
+export function cliResponseState(json) {
+  console.log('cliResponseState', json);
+  return {
+    type: CLI,
+    data: json,
+  }
+}
